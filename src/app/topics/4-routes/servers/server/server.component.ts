8e3d5c136980b4529e24dba5c3c8319e2c9f19ee
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2021-06-21 15:02:30 +0300

    practising child routes and query params

--- src/app/topics/4-routes/servers/server/server.component.ts
+++ src/app/topics/4-routes/servers/server/server.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ServersService } from '../servers.service';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router'
+import { ServersService } from '../servers.service'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-server',
@@ -7,13 +9,24 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  server: { id: number; name: string; status: string }
+  paramsSubscribtion: Subscription
 
-  server: {id: number, name: string, status: string};
+  constructor (
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  constructor(private serversService: ServersService) { }
-
-  ngOnInit() {
-    this.server = this.serversService.getServer(1);
+  ngOnInit (): void {
+    const serverId = +this.route.snapshot.params['id']
+    this.server = this.serversService.getServer(serverId)
+    this.paramsSubscribtion = this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id'])
+    })
   }
 
+  onEdit () {
+    this.router.navigate(['edit'], { relativeTo: this.route,queryParamsHandling: 'preserve' })
+  }
 }
--- src/app/topics/4-routes/users/user/user.component.ts
+++ src/app/topics/4-routes/users/user/user.component.ts
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs'
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit,OnDestroy {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string }
   paramsSubscription: Subscription
 
@@ -18,15 +18,12 @@ export class UserComponent implements OnInit,OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
-    this.paramsSubscription = this.route.params.subscribe(
-      (params: Params) => {
-        this.user.id = params['id'];
-        this.user.name = params['name'];
-      }
-    )
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id']
+      this.user.name = params['name']
+    })
   }
-  ngOnDestroy() {
+  ngOnDestroy () {
     this.paramsSubscription.unsubscribe()
   }
-
 }
